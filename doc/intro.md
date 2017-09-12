4ad2671f3be9374b411b067116438b0731f2e93c
Nick Rothwell <nick@Goztepe.local>
2017-09-12 21:36:09 +0100

    Initial build.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to lifecycle
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject net.cassiel/lifecycle "0.1.0-SNAPSHOT"
+  :description "Trivial wrapper around Component https://github.com/stuartsierra/component"
+  :url "https://github.com/cassiel/lifecycle"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [com.stuartsierra/component "0.3.2"]])
